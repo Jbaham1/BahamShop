adf260f1e28aab3446c8324544752fff41c881bf
Jeremiah Baham <44992378+Jbaham1@users.noreply.github.com>
2021-11-25 11:56:35 -0600

    home set up

---
+++ src/main/java/com/ecommerce/bahamshop/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.ecommerce.bahamshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String welcomeDefault(){
+        return "home";
+    }
+}
